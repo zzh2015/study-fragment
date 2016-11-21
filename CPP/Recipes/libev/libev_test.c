067e7548647c4cf38b362e9c1fc287f5b2cb21fb
zengzhaohu <zengzhaohu@integritytech.com.cn>
2016-11-21 20:20:44 +0800

    libev usage

---
+++ CPP/Recipes/libev/libev_test.c
@@ -0,0 +1,39 @@
+/*************************************************************************
+ > File Name: libev_test.c
+ > Author:
+ > Mail:
+ > Created Time: 2016年11月21日 星期一 16时44分59秒
+ ************************************************************************/
+#include <ev.h>
+#include <stdio.h>
+
+ev_io stdin_watcher;
+ev_timer timeout_watcher;
+
+static void stdin_cb (EV_P_ ev_io *w, int revents) {
+    puts("stdin ready");
+
+    ev_io_stop(EV_A_ w);
+
+    ev_break(EV_A_ EVBREAK_ALL);
+}
+
+static void timeout_cb (EV_P_ ev_timer *w, int revents) {
+    puts("timeout");
+
+    ev_break (EV_A_ EVBREAK_ONE);
+}
+
+int main() {
+    struct ev_loop *loop = EV_DEFAULT;
+
+    ev_io_init (&stdin_watcher, stdin_cb, 1, EV_READ);
+    ev_io_start (loop, &stdin_watcher);
+
+    ev_timer_init(&timeout_watcher, timeout_cb, 5.5, 0.);
+    ev_timer_start(loop, &timeout_watcher);
+
+    ev_run(loop, 0);
+
+    return 0;
+}
