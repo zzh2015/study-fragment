a1c1064578c827fad00d73b90ad04f69b4c9bc10
zengzhaohu <zengzhaohu@integritytech.com.cn>
2017-01-11 10:03:59 +0800

    golang code

--- Golang/data_type.go
+++ Golang/data_type.go
@@ -24,4 +24,17 @@ func main() {
     ss1 := ss[1:4]
     fmt.Println(ss)
     fmt.Println(ss1)
+
+    // 数组
+    // 定义多维数组，只有第一维允许使用[...]
+    // len,cap返回第一维度长度
+    // 复制，会复制整个数组
+    var arr1 [5]int // zero
+    arr2 := [4]int{2, 5}
+    arr3 := [4]int{5, 3: 10}
+    arr4 := [...]int{1, 2, 3}
+    arr5 := [...]int{10; 3: 100}
+    fmt.Println(arir1, arr2, arr3, arr4, arr5)
+
+    // 切片本身只是一个只读对象
 }
